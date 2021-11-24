164e079b73f5f11de17ad9146b681abc6de09e64
MarineV <57832170+mantarina@users.noreply.github.com>
2021-11-24 15:32:54 +0100

    Update streamlit-recommendation-movie.py

--- streamlit-recommendation-movie.py
+++ streamlit-recommendation-movie.py
@@ -1,7 +1,7 @@
 import pandas as pd 
 import numpy as np 
 import streamlit as st
-from sklearn.neighbors import KNeighborsClassifier, NearestNeighbors
+from sklearn.neighbors import NearestNeighbors
 from sklearn.preprocessing import StandardScaler
 
 
